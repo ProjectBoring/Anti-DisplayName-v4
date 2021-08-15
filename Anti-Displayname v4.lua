d916b9291f0f42fd28be537872e65606b8e5f322
ProjectBoring <73672695+ProjectBoring@users.noreply.github.com>
2021-08-15 14:24:02 -0500

    Update Anti-Displayname v4.lua

--- Anti-Displayname v4.lua
+++ Anti-Displayname v4.lua
@@ -1,1 +1,9 @@
-print('moved script, check thread: https://v3rmillion.net/showthread.php?tid=1131211')
+game:GetService('StarterGui'):SetCore('SendNotification',{
+	Title = 'Mothra';
+	Text = 'Moved to pastebin! Check the v3rm thread which has been set to clipboard.';
+	Duration = 20;
+})
+
+if setclipboard then
+    setclipboard('https://v3rmillion.net/showthread.php?tid=1131211')
+end
